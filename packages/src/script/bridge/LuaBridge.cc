1f0ba4151bd26283159505ce0b3873da64b662ee
w30029682 <wangjinfan1@huawei.com>
2024-08-10 17:28:48 +0800

    #feat function fix

--- packages/src/script/bridge/LuaBridge.cc
+++ packages/src/script/bridge/LuaBridge.cc
@@ -333,8 +333,9 @@ core::AutoPtr<Value> LuaBridge::load(lua_State *state, int index) {
     lua_getfield(state, index, "$handle");
     if (lua_type(state, -1) == LUA_TNUMBER) {
       uint32_t handle = lua_tonumber(state, -1);
-      value =
-          ctx->getNativeGlobal()->getField(ctx, "$objects")->getIndex(ctx, handle);
+      value = ctx->getNativeGlobal()
+                  ->getField(ctx, "$objects")
+                  ->getIndex(ctx, handle);
     } else {
       static int obj_idx = 0;
       value->setObject(ctx);
@@ -349,16 +350,24 @@ core::AutoPtr<Value> LuaBridge::load(lua_State *state, int index) {
     }
   } break;
   case LUA_TFUNCTION: {
-    static int func_idx = 0;
-    value->setObject(ctx);
-    value->setRawField(ctx, "$handle",
-                       ctx->createValue()->setNumber(ctx, func_idx));
-    value->setMetadata(getFunctionMetadata());
-    lua_getglobal(state, "$functions");
-    auto objects = lua_gettop(state);
-    lua_pushvalue(state, index);
-    lua_seti(state, objects, func_idx);
-    func_idx++;
+    lua_getfield(state, index, "$handle");
+    if (lua_type(state, -1) == LUA_TNUMBER) {
+      uint32_t handle = lua_tonumber(state, -1);
+      value = ctx->getNativeGlobal()
+                  ->getField(ctx, "$functions")
+                  ->getIndex(ctx, handle);
+    } else {
+      static int func_idx = 0;
+      value->setObject(ctx);
+      value->setRawField(ctx, "$handle",
+                         ctx->createValue()->setNumber(ctx, func_idx));
+      value->setMetadata(getFunctionMetadata());
+      lua_getglobal(state, "$functions");
+      auto objects = lua_gettop(state);
+      lua_pushvalue(state, index);
+      lua_seti(state, objects, func_idx);
+      func_idx++;
+    }
   } break;
   default:
     break;
